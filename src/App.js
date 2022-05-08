4d524664c29246dcdc3d79c9905509256a5f04c0
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-05-08 23:05:23 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Add_word';
+import './components/Card';
+import './components/Delete_word';
+import './components/Dictionary';
+import './components/Edit_word';
+import './components/Footer';
+import './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
