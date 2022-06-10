68ec20b1df2f9b688d469f309d9bacf768920302
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-06-10 15:14:23 +0300

    add table

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Dictionary from './components/Dictionary';
 import Edit_word from './components/Edit_word';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Table from './components/Table'
+import Table from './components/Table';
+import Table from '/components/Table.module.css'
 
 
 
 function App() {
   return (
     <div className="App">
+      <div className="Table">
       <Table></Table>
+      </div>
     </div>
   );
 }
